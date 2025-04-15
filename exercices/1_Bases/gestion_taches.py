8e409437e39b5c5255075a0486e765292e4af2b7
unknown <kylian.chebbah@stimplus.com>
2025-04-15 18:05:27 +0200

    Refactor: Ajout fonction clear_console

--- exercices/1_Bases/gestion_taches.py
+++ exercices/1_Bases/gestion_taches.py
@@ -1,5 +1,6 @@
-import time
-import os
+import time 
+import os #Permet l'execution des commandes systemes en fonction de l'OS
+
 def affficher_menu():
     print("----- Menu ------")
     print("1. Afficher les taches")
@@ -22,8 +23,15 @@ def ajouter_tache(liste,tache):
     
 def supprimer_tache(liste,tache):
     liste.pop(tache-1)
-    
-    
+
+#Fonction pour executer la commande en fonction de l'os  
+def clear_console():
+    if os.name == "nt":
+        os.system('cls')
+    else:
+        os.system('clear')
+        
+# Programme de gestion des taches avec menu
 def programme(liste):
     
     while True:
@@ -31,38 +39,37 @@ def programme(liste):
         choix = choix_utilisateur()
         
         if choix == 1:
-            os.system('cls')
+            clear_console()
             if len(liste) == 0:
                 print("La liste est vide")
                 afficher_taches(liste)
                 time.sleep(2)
-                os.system('cls')
+                clear_console()
             else:
                 afficher_taches(liste)
                 time.sleep(2)
-                os.system('cls')
+                clear_console()
         elif choix == 2:
-            os.system('cls')
+            clear_console()
             afficher_taches(liste)
             tache = input("Entrez la tache à ajouter: ")
             ajouter_tache(liste,tache)
             print("La tache à été ajouté")
             time.sleep(2)
-            os.system('cls')
+            clear_console()
         elif choix == 3:
-            os.system('cls')
+            clear_console()
             afficher_taches(liste)
             tache = int(input("Entrez la tache à supprimer: "))
             supprimer_tache(liste,tache)
             print("La tache à été supprimé")
             time.sleep(2)
-            os.system('cls')
+            clear_console()
         elif choix == 4:
-            os.system('cls')
-            afficher_taches(taches)
+            clear_console()
             print("Vous allez quitter le programme")
             time.sleep(2)
-            os.system('cls')
+            clear_console()
             break
         else:
             print("Entrez un nombre entre 1 à 4")
