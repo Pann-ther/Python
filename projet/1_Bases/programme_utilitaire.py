9d69ae2d07680872e8a36a8cd210c352911e809b
unknown <kylian.chebbah@stimplus.com>
2025-04-25 13:24:18 +0200

    add: fonctions d'affichages des menus

---
+++ projet/1_Bases/programme_utilitaire.py
@@ -0,0 +1,20 @@
+def afficher_menu_principal():
+    print("--------Menu Principal--------")
+    print("1. Calculatrice")
+    print("2. Convertisseur d'unité")
+    print("3. Quitter le programme")
+    
+    
+def afficher_menu_calculatrice():
+    print("--------Calculatrice--------")
+    print("1. Addition")
+    print("2. Soustraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("Retour au menu principal")
+    
+def afficher_menu_conversion():
+    print("--------Convertisseur d'unites--------")
+    print("1. Kilometre en metre")
+    print("2. Celsuis en Fehrenheit")
+    print("3. Retour au menu principal")
