4d9e7fca9284e0d17149ddd3c4680318d832703f
unknown <kylian.chebbah@stimplus.com>
2025-04-25 13:52:43 +0200

    add: ajout des fonctions de calculette et conversion

--- projet/1_Bases/programme_utilitaire.py
+++ projet/1_Bases/programme_utilitaire.py
@@ -4,9 +4,7 @@ def afficher_menu_principal():
     print("2. Convertisseur d'unité")
     print("3. Quitter le programme")
     return int(input("Votre choix: "))
-    
-    
-    
+ 
 def afficher_menu_calculatrice():
     print("--------Calculatrice--------")
     print("1. Addition")
@@ -16,10 +14,24 @@ def afficher_menu_calculatrice():
     print("5. Retour au menu principal")
     return int(input("Choisissez l'operation: "))
     
-    
 def afficher_menu_conversion():
     print("--------Convertisseur d'unites--------")
     print("1. Kilometre en metre")
     print("2. Celsuis en Fehrenheit")
     print("3. Retour au menu principal")
     return int(input("Choisissez le type de conversion: "))
+
+def saisieValeur(msg):
+    return float(input(f"Entrez {msg}: "))
+
+def addition(a,b):
+    return a+b
+
+def soustraction(a,b):
+    return a-b
+
+def multiplication(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
