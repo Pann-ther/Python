f95157ed610d47c2a2d105a091fb53b33eac623c
unknown <kylian.chebbah@stimplus.com>
2025-05-06 17:26:33 +0200

    push du code en cours

--- projet/2_Structures_avancees/gestionnaire_contacts.py
+++ projet/2_Structures_avancees/gestionnaire_contacts.py
@@ -1,5 +1,12 @@
 import csv
-
+def existance_utilisateur(prenom):
+    with open("contacts.csv","r") as fichier:
+        lecteur = csv.reader(fichier)
+        for line in lecteur:
+            if line[0] == prenom:
+                return True
+        return False
+    
 def afficher_menu():
     print("------Menu------")
     print("1. Afficher les contacts")
@@ -13,13 +20,48 @@ def afficher_menu():
 def afficher_contacts():
     with open("contacts.csv","r") as fichier:
         contenu = csv.DictReader(fichier)
-        print(contenu)
+        for line in contenu:
+            print(f"Prenom : {line["Prenom"]}, Nom: {line["Nom"]}, Telephone: {line["Telephone"]}, Email: {line["Email"]}")
 
 def ajouter_contact():
-    pass
+    prenom = input("Entrez le prenom du contact")
+    nom = input("Entrez le nom du contact")
+    telephone = input("Entrez le numero de telephone du contact")
+    mail = input("Entrez le mail du contact")
+    donnees = [prenom, nom, telephone, mail]
+    
+    with open("contact.csv","w", newline="") as fichier:
+        ajout = csv.writer(fichier)
+        ajout.writerow(donnees)
 
 def supprimer_contact():
-    pass
+    prenom = input("Entrez le prenom du contact que vous souhaitez supprimer ")
+    
+    if not existance_utilisateur(prenom):
+        with open("contacts.csv","r") as fichier:
+            buffer = list(csv.reader(fichier))
+            
+        contact_maj = [ligne for ligne in buffer if buffer[0] != prenom]
+        
+        with open("contacts.csv","w") as fichier:
+            ecriture = csv.writer(fichier)
+            ecriture.writerows(contact_maj)
+            
+        print(f"{prenom} a bien ete supprime des contacts")
+    else:
+        print(f"{prenom} n'a pas ete trouve dans les contacts")
+    
+    
 
 def modifier_contact():
-    pass
+    prenom = input("Entrez le prenom du contact que duquel vous souhaitez modifier les valeurs")
+    nom = input("Entrez le nom du contact que duquel vous souhaitez modifier les valeurs")
+    champs = input("Entrez le champs que vous souhaitez modifier")
+    valeur = input("Entrez la nouvelle valeur ")
+    
+    with open("contrats.csv","r") as fichier:
+        lecteur = csv.reader(fichier)
+        
+        for line in lecteur:
+            line[] = 
+    
