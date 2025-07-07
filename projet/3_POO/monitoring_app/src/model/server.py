cc3dcee885cf65a73e597418b646c602cb749947
unknown <kylian.chebbah@stimplus.com>
2025-07-07 17:49:56 +0200

    Feat: ajout de la classe Server

---
+++ projet/3_POO/monitoring_app/src/model/server.py
@@ -0,0 +1,34 @@
+import monitored_device as md
+class Server(md.MonitoredDevice):
+    
+    def __init__(self,name, ip, location,status,state, cpu_usage, ram_usage, disk_usage, id=None):
+        super().__init__(name,ip,location,status,state,cpu_usage,id=None)
+        self.ram_usage = ram_usage
+        self.disk_usage = disk_usage
+        
+    # Getters
+    
+    def get_ram_usage(self):
+        return self.ram_usage
+    
+    def get_disk_usage(self):
+        return self.disk_usage
+    
+    def get_disk_free(self):
+        return self.disk_free
+    
+    # Setters
+    def set_ram_usage(self, usage):
+        self.ram_usage = usage
+        
+    def set_disk_usage(self, usage):
+        self.disk_usage = usage
+        
+    # Gestion de l'actualisation des valeurs dynamiques
+    def update_ram_usage(self):
+        new_value = md.MonitoredDevice.generate_random_value(20,20,self.ram_usage())
+        self.ram_usage = max(0,min(100,new_value))
+        
+    def update_disk_usage(self):
+        new_value = md.MonitoredDevice.generate_random_value(20,20,self.disk_usage())
+        self.disk_usage = max(0, min(100, new_value))
